a60a4cdc31bc3808c8c9a6dd14b931e44261160b
Roddy <roddy_daly110@hotmail.com>
2018-12-01 10:30:08 +0000

    first commit

---
+++ src/main/java/com/daly_roddy/API/Development/ApiDevelopmentApplication.java
@@ -0,0 +1,12 @@
+package com.daly_roddy.API.Development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDevelopmentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/daly_roddy/API/Development/ApiDevelopmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daly_roddy.API.Development;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiDevelopmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
